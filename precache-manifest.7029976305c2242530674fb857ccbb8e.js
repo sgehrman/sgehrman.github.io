26643ffe8493c2cef619d92a6765fff9a04dd0b9
Steve Gehrman <sgehrman@gmail.com>
2020-09-26 02:34:54 -0700

    first

---
+++ precache-manifest.7029976305c2242530674fb857ccbb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3dbe111b5bf9053544ef777b8b1fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3955a7a55a944cd87f21",
+    "url": "/static/css/main.cd5a7a37.chunk.css"
+  },
+  {
+    "revision": "caf49af2472091da046b",
+    "url": "/static/js/2.57e1ef75.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.57e1ef75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3955a7a55a944cd87f21",
+    "url": "/static/js/main.3123f4e4.chunk.js"
+  },
+  {
+    "revision": "01aae708545307ba55e8",
+    "url": "/static/js/runtime-main.0e272397.js"
+  }
+]);
